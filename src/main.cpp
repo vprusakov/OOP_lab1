8d177d72e64fbdb9afc3c8f2f7e16c31bc2b4644
Macbook Air <macbookair@MacBook-Air-Vaceslav.local>
2017-09-20 17:35:18 +0300

    .gitignore corrected

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "Header.h"
+
+int main() {
+    IniParser p;
+    p.Initialize("input_files/lab01_input_correct_09.ini");
+    p.Parse();
+    cout << p.IsHaveParam("DEBUG", "CheckThisParameter");
+    //cout << p.IsHaveParam("COMMON", "LogNCMD") << endl;
+    system("pause");
+    return 0;
+}
