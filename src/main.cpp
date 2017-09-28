820d24b97471a1d5beae7ced6e08dcfcafa13fb2
Macbook Air <macbookair@MacBook-Air-Vaceslav.local>
2017-09-28 19:43:25 +0300

    lab1

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 
 int main() {
     IniParser p;
-    cout << "hello world" << endl;
     p.Initialize("input_files/lab01_input_correct_09.ini");
     p.Parse();
     cout << p.IsHaveParam("DEBUG", "CheckThisParameter");
