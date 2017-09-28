c309c3899591b7f00a79704f4449bbebb6a9ac4c
Вячеслав Прусаков <vyacheslav.prusakov@gmail.com>
2017-09-28 22:08:30 +0300

    cleaning

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,12 @@
 
 int main() {
     IniParser p;
+	cout << "hello" << endl;
     p.Initialize("input_files/lab01_input_correct_09.ini");
     p.Parse();
-    cout << p.IsHaveParam("DEBUG", "CheckThisParameter");
+    cout << p.IsHaveParam("DEBUG", "CheckThisParameter") << " ";
+	p.PrintAll();
+
     //cout << p.IsHaveParam("COMMON", "LogNCMD") << endl;
     system("pause");
     return 0;
